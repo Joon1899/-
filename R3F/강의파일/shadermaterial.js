82782235a588079fe34ce7841f45bb65362abf72
Joon1899 <102898911+Joon1899@users.noreply.github.com>
2024-05-19 21:33:24 +0900

    Create shadermaterial.js

---
+++ R3F/강의파일/shadermaterial.js
@@ -0,0 +1,45 @@
+import { MeshDiscardMaterial, OrbitControls, shaderMaterial} from "@react-three/drei"
+import * as THREE from "three"
+import {extend} from "@react-three/fiber"
+
+const SimpleMaterial = new shaderMaterial({
+uColor:new THREE.Color(1,0,0)
+}, 
+`
+varying vec2 vUv;
+void main(){
+  vUv = uv;
+  gl_Position=projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
+`,`
+uniform vec3 uColor;
+varying vec2 vUv;
+void main(){
+  gl_FragColor = vec4(vUv.y * uColor, 1.0)
+}`
+
+
+)
+extend({SimpleMaterial})
+function MyElement3D(){
+
+  
+  
+  return(
+
+  <>
+  <OrbitControls />
+  <ambientLight intensitiy={0.2} />
+  <directionalLight position={[0, 1, 0]}/>
+  <directionalLight position={[1, 2, 8]} intensity={0.7}/>
+
+<mesh>
+  <boxGeometry/>
+  <simpleMaterial uColor={"green"}/>
+</mesh>
+ 
+
+  </>
+  )
+}
+export default MyElement3D
