c3bfd20b74237b0aa0c1d2b6b255a9ca0ee91186
Joon1899 <102898911+Joon1899@users.noreply.github.com>
2024-05-19 21:31:33 +0900

    Create meshMatcapMaterial.js

---
+++ R3F/강의파일/meshMatcapMaterial.js
@@ -0,0 +1,44 @@
+import { OrbitControls, useTexture } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+import { useControls } from "leva";
+import * as THREE from "three"
+
+function MyElement3D() {
+    const mesh1 = useRef()
+    const mesh2 = useRef()
+
+    useEffect(() => {
+        mesh2.current.material = mesh1.current.material
+    }, [])
+
+    const matcap = useTexture("./public/images/matcap.jpg")
+
+    return (
+        <>
+            <OrbitControls />
+
+            <ambientLight intensity = {0.2} />
+
+            <mesh 
+                ref = {mesh1}
+                position = {[0.7, 0, 0]}
+            >
+                <torusKnotGeometry args = {[0.5, 0.15, 256, 128]} />
+
+                <meshMatcapMaterial matcap={matcap} 
+                    flatShading = {true}
+                />
+                
+            </mesh>
+
+            <mesh 
+                ref = {mesh2}
+                position = {[-0.7, 0, 0]}
+            >
+                <torusGeometry args={[0.5, 0.2]} />
+            </mesh>
+        </>
+    )
+}
+
+export default MyElement3D
